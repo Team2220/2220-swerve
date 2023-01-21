1c431da8c6817743b81b327a4b3c962eb9c61fdd
sarahpengG <123267535+sarahpengG@users.noreply.github.com>
2023-01-21 14:13:14 -0600

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -3,6 +3,10 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -10,6 +14,10 @@ public class Arm extends SubsystemBase {
 
     private TalonFX wrist = new TalonFX(Constants.WRIST_TALONFX);
     private TalonFX shoulder = new TalonFX(Constants.SHOULDER_TALONFX);
+
+    ShuffleboardTab arm = Shuffleboard.getTab("arm");
+    GenericEntry shoulderSB = arm.add("shoulder angle", 0).getEntry();
+    GenericEntry wristSB = arm.add("wrist angle", 0).getEntry();
 
     public Arm() {
         wrist.configVoltageCompSaturation(10);
@@ -20,7 +28,6 @@ public class Arm extends SubsystemBase {
         supplyConfig.enable = true;
         shoulder.configSupplyCurrentLimit(supplyConfig);
         wrist.configSupplyCurrentLimit(supplyConfig);
-
 
         StatorCurrentLimitConfiguration config = new StatorCurrentLimitConfiguration();
         config.currentLimit = 10;

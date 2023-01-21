543ae77b0bab3c1999cb3380084c6d937f794ad8
notJithin <123132870+notJithin@users.noreply.github.com>
2023-01-21 13:44:37 -0600

    deleted example subsystems : )

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
 
   private final Swerve m_swerve;
   private final Arm m_arm;
@@ -55,7 +55,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
+    
     return null;
   }
 }

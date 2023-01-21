813d419cc9b3494db9984b2e1242d93db914bb2a
sarahpengG <123267535+sarahpengG@users.noreply.github.com>
2023-01-21 17:01:53 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,4 +28,5 @@ public final class Constants {
     public static final double SHOULDER_ENCODER_OFFSET = 0.0;
     public static final double SHOULDER_GEAR_RATIO = (5.0 / 1.0) * (5.0 / 1.0) * (4.0 / 1.0) * (15.0 / 54.0);
     public static final double WRIST_GEAR_RATIO = (5.0 / 1.0) * (5.0 / 1.0) * (5.0 / 1.0);
+    public static final double TALONFX_ENCODER_TICKS = 2048;
 }

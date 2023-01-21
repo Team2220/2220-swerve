b58c715a73cea057fd6e2c921d09aed56b2f79e5
notJithin <123132870+notJithin@users.noreply.github.com>
2023-01-21 14:17:51 -0600

    DUTY encoder on arms

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -3,14 +3,20 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Arm extends SubsystemBase {
-
+    private DutyCycleEncoder wristEncoder = new DutyCycleEncoder(Constants.WRIST_DUTYENCODER);
+    private DutyCycleEncoder armEncoder = new DutyCycleEncoder(Constants.SHOULDER_DUTYENCODER); 
+       
     private TalonFX wrist = new TalonFX(Constants.WRIST_TALONFX);
     private TalonFX shoulder = new TalonFX(Constants.SHOULDER_TALONFX);
-
+   
     public Arm() {
         wrist.configVoltageCompSaturation(10);
         shoulder.configVoltageCompSaturation(10);

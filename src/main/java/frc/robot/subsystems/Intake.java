995b6ea23ae6e3350a3f495a6f1490cb40b06b7e
notJithin <123132870+notJithin@users.noreply.github.com>
2023-01-21 11:51:32 -0600

    Added intake motors. hi.

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class Intake {
+    private TalonFX left = new TalonFX(1);
+    private TalonFX right = new TalonFX(2);
+    
+}

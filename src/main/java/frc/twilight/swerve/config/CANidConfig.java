ef383cadc2e61f05f563de136012ddd87004793a
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2023-02-17 20:11:12 -0600

    Update CANidConfig.java

--- src/main/java/frc/twilight/swerve/config/CANidConfig.java
+++ src/main/java/frc/twilight/swerve/config/CANidConfig.java
@@ -3,36 +3,36 @@ package frc.twilight.swerve.config;
 public class CANidConfig {
   // CAN IDs for the front left swerve module
   // Drive motor
-  public static final int DT_FL_DM = 14;
+  public static final int DT_FL_DM = 12;
   // Steer motor
-  public static final int DT_FL_SM = 13;
+  public static final int DT_FL_SM = 11;
 
   // Steer PWM encoder (RoboRIO)
   public static final int DT_FL_SE = 1;
 
   // CAN IDs for the front right swerve module
   // Drive motor
-  public static final int DT_FR_DM = 16;
+  public static final int DT_FR_DM = 18;
   // Steer motor
-  public static final int DT_FR_SM = 15;
+  public static final int DT_FR_SM = 17;
 
   // Steer PWM encoder (RoboRIO)
   public static final int DT_FR_SE = 2;
 
   // CAN IDs for the back left swerve module
   // Drive motor
-  public static final int DT_BL_DM = 18;
+  public static final int DT_BL_DM = 16;
   // Steer motor
-  public static final int DT_BL_SM = 17;
+  public static final int DT_BL_SM = 15;
 
   // Steer PWM encoder (RoboRIO)
   public static final int DT_BL_SE = 3;
 
   // CAN IDs for the back right module
   // Drive motor
-  public static final int DT_BR_DM = 12;
+  public static final int DT_BR_DM = 14;
   // Steer motor
-  public static final int DT_BR_SM = 11;
+  public static final int DT_BR_SM = 13;
 
   // Steer PWM encoder (RoboRIO)
   public static final int DT_BR_SE = 0;
